c089591ac0879e77daac80240d13920498293538
jinsik kim <rlawlstlr9312@gmail.com>
2020-10-05 12:10:49 -0600

    reactwo

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,21 @@
 import React, { Component } from 'react';
 import './app.css';
-import Review from './reviews';
+
 
 class App extends Component {
-    render() {
-      return (
-        <div className={"App"}>
-      <Review />
-    </div>
-  ) 
-};
+  render() {
+    return (
+      <div className="App">
+        <header className="title"> 
+        <h1>Customer ratings and reviews</h1>
+        <h2>Average rate</h2>
+        <h3>Reviews</h3>
+        </header>
+        <button className="type">submit review</button>
+      </div>
+    );
+  
 }
+};
 
 export default App;
