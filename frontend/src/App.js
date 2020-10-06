c679079627b8f9efd3f20c848b1f624f29643a15
jinsik kim <rlawlstlr9312@gmail.com>
2020-10-06 17:06:24 -0600

    UPDATED

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,38 +5,42 @@ import './app.css';
 class App extends Component {
   consturctor(props) {
     this.state = {
-      items: [],
+      data: [],
+      info: null,
       isLoaded: false,
     }
   }
 
 
   componentDidMount() {
-
-    fetch('/targets/')
+    fetch('/targets/1')
     .then(res => {
       return res.json();
     }).then(data => {
-      console.log(data.rows)
-    })
+      this.setState({info: data});
+      });
   }
-handleClick() {
-  console.log('Clicked');
-}
+
+
+  
+  handleClick() {
+    console.log('Clicked');
+  }
   render() {
     return (
       <div className="App">
         <header className="title"> 
-        <h1>Customer ratings and reviews</h1>
-        <h2>Average rate</h2>
-        <p>4.7</p>
-        <h3>Reviews</h3>
+        <h1 className="title">Customer ratings and reviews</h1>
+        <h2 className="rate">Average rate</h2>
+        <h3 className="photo">Review photo</h3>
+        <h3 className="comment">Reviews</h3>
         </header>
+        <div className="container"></div>
         <button className="type" onClick={ this.handleClick.bind(this) }>submit review</button>
       </div>
     );
+    
+  }
   
 }
-};
-
 export default App;
