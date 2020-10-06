132d86495245e97496e32e94a13b21bcd9d8b1f9
jinsik kim <rlawlstlr9312@gmail.com>
2020-10-06 08:22:24 -0600

    Updated

--- db/queries.js
+++ db/queries.js
@@ -1,11 +1,13 @@
 // Connect to your database
+const { Pool } = require('pg');
 
+const pool = new Pool ({
+user: process.env.DATA_USER,
+host: process.env.DATA_HOST,
+database: process.env.DATA_DATABASE,
+password: process.env.DATA_PASS,
+port: process.env.DATA_PORT
+});
 
-// Your database query here
+module.exports = pool;
 
-
-
-// Don't forget to export your functions
-module.exports = {
-  
-};
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,15 +3,36 @@ import './app.css';
 
 
 class App extends Component {
+  consturctor(props) {
+    this.state = {
+      items: [],
+      isLoaded: false,
+    }
+  }
+
+
+  componentDidMount() {
+
+    fetch('/targets/')
+    .then(res => {
+      return res.json();
+    }).then(data => {
+      console.log(data.rows)
+    })
+  }
+handleClick() {
+  console.log('Clicked');
+}
   render() {
     return (
       <div className="App">
         <header className="title"> 
         <h1>Customer ratings and reviews</h1>
         <h2>Average rate</h2>
+        <p>4.7</p>
         <h3>Reviews</h3>
         </header>
-        <button className="type">submit review</button>
+        <button className="type" onClick={ this.handleClick.bind(this) }>submit review</button>
       </div>
     );
   
