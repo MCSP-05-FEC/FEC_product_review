d56c65e1e57577ad32c9e32bf9647951d0bc78c0
jinsik kim <rlawlstlr9312@gmail.com>
2020-10-07 17:13:31 -0600

    OCT7th

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,42 +1,68 @@
 import React, { Component } from 'react';
 import './app.css';
+import BeautyStars from 'beauty-stars';
 
 
 class App extends Component {
-  consturctor(props) {
+  constructor(props) {
+    super(props)
     this.state = {
       data: [],
-      info: null,
+      info: {rating: 5},
+      number: {value: 5},
       isLoaded: false,
     }
   }
-
+  
 
   componentDidMount() {
     fetch('/targets/1')
     .then(res => {
-      return res.json();
-    }).then(data => {
-      this.setState({info: data});
-      });
+      return (res.json());
+     }).then(data => {
+      console.log(data);
+      this.setState({info: data.rows[0]})
+      }); 
   }
 
-
-  
   handleClick() {
-    console.log('Clicked');
+    const id= this.state.id;
+    fetch('/targets/'+id)
+    .then(res => {
+      return (res.json());
+     }).then(data => {
+      console.log(data);
+      this.setState({info: data.rows[0]})
+      });
+       
   }
+
+
+
   render() {
     return (
       <div className="App">
         <header className="title"> 
-        <h1 className="title">Customer ratings and reviews</h1>
-        <h2 className="rate">Average rate</h2>
-        <h3 className="photo">Review photo</h3>
-        <h3 className="comment">Reviews</h3>
+        <h1 className="title">Guest ratings and reviews</h1>
+         <h2 className="rate">Average rate: {this.state.info.rating}</h2>
+         <div className="wrapper">
+        <h1 style={{ textAlign: "center", color: "#fff" }}>Beauty Stars</h1>
+        <BeautyStars
+          value={this.state.value}
+          onChange={value => this.setState({ value })}
+        />
+      </div>
+    <h3 className="photo">Review photo:
+    <img src={this.state.info.image}></img>
+    </h3>
+    <h3 className="comment">Item name: {this.state.info.name} </h3>
         </header>
-        <div className="container"></div>
-        <button className="type" onClick={ this.handleClick.bind(this) }>submit review</button>
+    <div className="container">
+      <h1 className="tittle">Reviews </h1>
+      <h3>{this.state.info.account}:</h3>
+      <h2>{this.state.info.comment}</h2>
+    </div>
+        <button className="type" onClick={ this.handleClick.bind(this) }>next</button>
       </div>
     );
     
